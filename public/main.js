4ad487d08084cb61824fd1b7aa05493215bc4e63
Boris <a402539@yandex.ru>
2021-03-05 20:39:26 +0300

    2

--- public/main.js
+++ public/main.js
@@ -16639,8 +16639,16 @@
       // 'Q',[51.67255514839676,16.303710937500004],
       // [50.14874640066278,18.676757812500004],
       // 'T',[49.866316729538674,25.0927734375]], {
+      // квадратичная кривая Безье B(t)=(1-t)^2*P0+2*t*(1-t)*P1+t^2*P2, t=0..1
+      // B(0)=P0, B(1)=P2, B'(0)=-2*P0+2*P1, B'(1)=-2*P1+2*P2
+      // P[1]=P0[1]+B'(0)*(P[0]-P0[0])=P2[1]+B'(1)*(P[0]-P2[0])
+      // P[0]=(P0[1]-B'(0)*P0[0]-P2[1]+B'(1)*P2[0])/(B'(1)-B'(0))
+      // P[1]= P2[1]-B'(1)*P2[0]+B'(1)*(P0[1]-B'(0)*P0[0]-P2[1]+B'(1)*P2[0])/(B'(1)-B'(0))
+      // = (P2[1]-B'(1)*P2[0]))
 
-      var pathOne = leafletSrc.curve(['M', latlngs[0], 'Q', [latlngs[2][0] - 1, latlngs[2][1] + 2], latlngs[1], 'T', latlngs[3]], {
+      var pathOne = leafletSrc.curve(['M', latlngs[0], 'C', [latlngs[0][0], latlngs[0][1] + 2], [latlngs[1][0], latlngs[1][1] - 2], latlngs[1], 'C', [latlngs[1][0], latlngs[1][1] + 2], [latlngs[2][0], latlngs[2][1] - 2], latlngs[2], 'C', [latlngs[2][0], latlngs[2][1] + 2], [latlngs[3][0], latlngs[3][1] - 2], latlngs[3], 'C', [latlngs[3][0], latlngs[3][1] + 2], [latlngs[4][0], latlngs[4][1] - 2], latlngs[4] //'Q', latlngs[3], latlngs[4], latlngs[2]
+      //'T', latlngs[4]
+      ], {
         animate: 3000 // ,
         // renderer: canvasRenderer
 
