b68dc29c6fda6d1014faa74dff518e72a0d868ff
Serg <salekseev@mvs.group>
2021-03-06 10:37:19 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,26 +5,53 @@ import 'gmx-drawing/dist/gmxDrawing-src.js';
 import 'gmx-drawing/dist/gmxDrawing.css';
 
 import 'leaflet-curve/leaflet.curve.js';
+import {CONTROL_POINTS} from './const.js';
 
 window.addEventListener('load', async () => {
-	const contPoints = {
-		"type":"Feature",
-		"properties":{"name":"контрольные точки"},
-		"geometry":{"type":"LineString","coordinates":[[-11.25,45.213004],[11.25,40.178873],[20.566406,47.517201],[27.949219,44.715514],[49.746094,47.398349]]}
-	};
-	const latlngs = contPoints.geometry.coordinates.map(it => { return it.reverse(); });
-	// L.Dom
-    const map = L.map('map', {}).setView([55.45, 37.37], 4);
+	const cont = document.body;
+	const header = L.DomUtil.create('div', 'header', cont);
+	const leftMenu = L.DomUtil.create('div', 'leftMenu', cont);
+	const mapCont = L.DomUtil.create('div', 'map', cont);
+	const leftCont = L.DomUtil.create('span', 'left', header);
+	L.DomEvent.on(L.DomUtil.create('span', 'icon sidebar', leftCont), 'click', (ev) => {
+		const cList = leftMenu.classList;
+		if (cList.contains('opened')) {
+			cList.remove('opened');
+			mapCont.classList.remove('opened');
+		} else {
+			cList.add('opened');
+			mapCont.classList.add('opened');
+		}
+	});
+	const centerCont = L.DomUtil.create('span', 'center', header);
+	const playButton = L.DomUtil.create('span', 'icon play', centerCont);
+	const rightCont = L.DomUtil.create('span', 'right', header);
+	const playButton1 = L.DomUtil.create('span', 'icon play', rightCont);
+
+    const map = L.map(mapCont, {}).setView([42.779275360241904, 17.666015625000004], 4);
+	window._test = map;
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-	console.log('kjjj', map.gmxDrawing);
+
+	const latlngs = CONTROL_POINTS;
 	map.gmxDrawing.add(L.polyline(latlngs), {
 		lineStyle: {dashArray: [5, 5], color: 'red'},
 		pointStyle: {size:20, fillColor: 'red'}
 	});
+	const curveArr = ['M', latlngs[0]];
+	for (let i = 0, len = latlngs.length - 1; i < len; i++) {
+		let p = latlngs[i];
+		let p1 = latlngs[i + 1];
+		curveArr.push('C');
+		curveArr.push([p[0],	p[1]	+ 2]);
+		curveArr.push([p1[0],	p1[1]	- 2]);
+		curveArr.push(p1);
+	}
 
+	var pathOne = L.curve(curveArr).addTo(map);
+/*
 var customPane = map.createPane("customPane");
 var canvasRenderer = L.canvas({pane:"customPane"});
 customPane.style.zIndex = 399; // put just behind the standard overlay pane which is at 400
@@ -53,54 +80,5 @@ var pathOne = L.curve([
 	   // ,
 	   // renderer: canvasRenderer
    }).addTo(map);
-
-	
-/*
-    const dataManager = new Worker("dataManager.js");
-    
-    const dateEnd = Math.floor(Date.now() / 1000);
-    const testLayer = new CanvasLayer({
-        // dateBegin: dateEnd - 24 * 60 * 60,
-        // dateEnd,
-        dataManager
-    });
-
-    testLayer.addTo(map);
-
-    const moveend = () => {
-        const zoom = map.getZoom();
-        const sbbox = map.getBounds();
-        const sw = sbbox.getSouthWest();
-        const ne = sbbox.getNorthEast();
-        const m1 = L.Projection.Mercator.project(L.latLng([sw.lat, sw.lng]));
-        const m2 = L.Projection.Mercator.project(L.latLng([ne.lat, ne.lng]));
-    
-		dataManager.postMessage({
-			cmd: 'moveend',
-			zoom,
-			bbox: [m1.x, m1.y, m2.x, m2.y],
-            bounds: map.getPixelBounds(),
-		});
-	}; 
-	map.on('moveend', moveend);
-	dataManager.onmessage = msg => {
-		 // console.log('Main dataManager', msg.data);
-		const data = msg.data || {};
-		const {cmd, layerId, tileKey} = data;
-		switch(cmd) {
-			case 'rendered':
-				// if (data.bitmap) {
-				requestAnimationFrame(() => {
-					testLayer.rendered(data.bitmap);
-				});
-				// }
-				break;
-			default:
-				console.warn('Warning: Bad message from worker ', data);
-				break;
-		}
-
-	};
- 	moveend();
 */
 });
