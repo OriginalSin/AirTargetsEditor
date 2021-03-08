1c61168168d0cbb6ed83670806e2f572edc41a3a
Serg <salekseev@mvs.group>
2021-03-08 15:17:21 +0300

    add play button

--- src/index.js
+++ src/index.js
@@ -5,66 +5,58 @@ import 'gmx-drawing/dist/gmxDrawing-src.js';
 import 'gmx-drawing/dist/gmxDrawing.css';
 
 import './leaflet.curve.js';
-// import 'leaflet-curve/leaflet.curve.js';
-import {CONTROL_POINTS} from './const.js';
+import {prepareNodes, AMPLITUDE, CONTROL_POINTS} from './utils.js';
 
 window.addEventListener('load', async () => {
-	const cont = document.body;
-	const header = L.DomUtil.create('div', 'header', cont);
-	const leftMenu = L.DomUtil.create('div', 'leftMenu', cont);
-	const mapCont = L.DomUtil.create('div', 'map', cont);
-	const leftCont = L.DomUtil.create('span', 'left', header);
-	L.DomEvent.on(L.DomUtil.create('span', 'icon sidebar', leftCont), 'click', (ev) => {
-		const cList = leftMenu.classList;
-		if (cList.contains('opened')) {
-			cList.remove('opened');
-			mapCont.classList.remove('opened');
-		} else {
-			cList.add('opened');
-			mapCont.classList.add('opened');
-		}
-	});
-	const centerCont = L.DomUtil.create('span', 'center', header);
-	const playButton = L.DomUtil.create('span', 'icon play', centerCont);
-	const rightCont = L.DomUtil.create('span', 'right', header);
-	const playButton1 = L.DomUtil.create('span', 'icon play', rightCont);
+	
+	const nodes = prepareNodes(document.body);
 
-    const map = L.map(mapCont, {}).setView([42.779275360241904, 17.666015625000004], 4);
+    const map = L.map(nodes.mapCont, {}).setView([42.779275360241904, 17.666015625000004], 4);
+	nodes.mapCont._map = map;
 	window._test = map;
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-	const latlngs = CONTROL_POINTS;
+	let latlngs = CONTROL_POINTS;
 	map.gmxDrawing.add(L.polyline(latlngs), {
 		lineStyle: {dashArray: [5, 5], color: 'red'},
 		pointStyle: {size:20, fillColor: 'red'}
 	}).on('edit', ev => {
-		let it = ev.object;
-		// console.log('kkkk', ev);
-		let arr = it.rings[0].ring._getLatLngsArr().map(p => [p.lat, p.lng]);
-		refreshCurves(arr);
+		latlngs = ev.object.rings[0].ring._getLatLngsArr().map(p => [p.lat, p.lng]);
+		refreshCurves();
 	});
 
 	let pathOne;
-	function refreshCurves(arr) {
-		if (pathOne) {
-			map.removeLayer(pathOne);
+	function refreshCurves(flag) {
+		if (pathOne) { map.removeLayer(pathOne); }
+		if (flag) {
+			nodes.playButton.classList.add('disabled');
+			return;
 		}
-		const amplitude = 0.1;
-		const curveArr = ['M', arr[0]];
+		const target = nodes.targets._targets[nodes.targets._current];
+		// const target = nodes.targets._targets[nodes.targets._targets.length - 1];
+		let first = [];
+		if (target) {
+			let latLng = target.getLatLng();
+			first = [[latLng.lat, latLng.lng]];
+		}
+		const arr = first.concat(latlngs);
+		let p0 = arr[0];
+		const curveArr = ['M', p0];
 		for (let i = 0, len = arr.length - 1; i < len; i++) {
-			
 			let p1 = arr[i];
 			let p2 = arr[i + 1];
-			let p0 = i===0? p1 : arr[i-1];
-			let p3 = i===len-1? p2 : arr[i+2];
+			let p3 = i === len - 1 ? p2 : arr[i + 2];
 			curveArr.push('C');
-			curveArr.push([p1[0]+amplitude*(p2[0]-p0[0]), p1[1]+amplitude*(p2[1]-p0[1])]);
-			curveArr.push([p2[0]-amplitude*(p3[0]-p1[0]), p2[1]-amplitude*(p3[1]-p1[1])
-				]);
+			curveArr.push([p1[0] + AMPLITUDE * (p2[0] - p0[0]), p1[1] + AMPLITUDE * (p2[1] - p0[1])]);
+			curveArr.push([
+				p2[0] - AMPLITUDE * (p3[0] - p1[0]),
+				p2[1] - AMPLITUDE * (p3[1] - p1[1])
+			]);
 			curveArr.push(p2);
+			p0 = p1;
 		}
 
 		pathOne = L.curve(curveArr, {
@@ -74,58 +66,57 @@ window.addEventListener('load', async () => {
 		   // renderer: canvasRenderer
 		}).addTo(map);
 	}
-	refreshCurves(CONTROL_POINTS);
+	map._refreshCurves = refreshCurves;
 
 	let traceArr = [];
 	function traceCurves() {
-		pathOne.trace([0, 0.25, 0.75, 1]).forEach(i => {
-			let circle = L.circleMarker(i, {radius: 8, color: 'green'});
-			traceArr.push(circle);
-			circle.addTo(map);
-		});
-	}
-	L.DomEvent.on(playButton, 'click', (ev) => {
-		const cList = playButton.classList;
-		if (cList.contains('run')) {
-			cList.remove('run');
-			traceArr.forEach(it => {
-				map.removeLayer(it);
-			});
+		if (pathOne) {
+			// const target = targets[0];
+			const arr = [];
+			for (let i = 0; i <= 1; i+= 0.01) {
+				arr.push(i);
+			}
 			traceArr = [];
+
+			pathOne.trace(arr).forEach(i => {
+				traceArr.push(i);
+			});
+		}
+	}
+
+	let intId;
+	const disable = (cList) => {
+		console.log('ddd');
+		nodes.pauseButton.classList.add('disabled');
+		cList.remove('run');
+		clearInterval(intId);
+	}
+
+	L.DomEvent.on(nodes.playButton, 'click', (ev) => {
+		const target = nodes.targets._targets[nodes.targets._current];
+		// const target = nodes.targets._targets[nodes.targets._targets.length - 1];
+		const cList = ev.target.classList;
+		if (cList.contains('run')) {
+			disable(cList);
 		} else {
 			cList.add('run');
+			nodes.pauseButton.classList.remove('disabled');
 			traceCurves();
+			// let time = 0;
+			intId = setInterval(() => {
+				if (!nodes.pauseButton.classList.contains('run')) {
+					let latlng = traceArr.shift();
+					if (latlng) {
+						target.setLatLng(latlng);
+					} else {
+						disable(cList);
+						// console.log('ddd', latlng);
+						// nodes.pauseButton.classList.add('disable');
+						// cList.remove('run');
+						// clearInterval(intId);
+					}
+				}
+			}, 100);
 		}
 	});
-
-/*
-var customPane = map.createPane("customPane");
-var canvasRenderer = L.canvas({pane:"customPane"});
-customPane.style.zIndex = 399; // put just behind the standard overlay pane which is at 400
-
-// var pathOne = L.curve(['M',[50.14874640066278,14.106445312500002],
-					   // 'Q',[51.67255514839676,16.303710937500004],
-						   // [50.14874640066278,18.676757812500004],
-					   // 'T',[49.866316729538674,25.0927734375]], {
-// квадратичная кривая Безье B(t)=(1-t)^2*P0+2*t*(1-t)*P1+t^2*P2, t=0..1
-// B(0)=P0, B(1)=P2, B'(0)=-2*P0+2*P1, B'(1)=-2*P1+2*P2
-// P[1]=P0[1]+B'(0)*(P[0]-P0[0])=P2[1]+B'(1)*(P[0]-P2[0])
-// P[0]=(P0[1]-B'(0)*P0[0]-P2[1]+B'(1)*P2[0])/(B'(1)-B'(0))
-// P[1]= P2[1]-B'(1)*P2[0]+B'(1)*(P0[1]-B'(0)*P0[0]-P2[1]+B'(1)*P2[0])/(B'(1)-B'(0))
-// = (P2[1]-B'(1)*P2[0]))
-
-var pathOne = L.curve([
-	'M', latlngs[0],
-	'C', [latlngs[0][0], latlngs[0][1] + 2], [latlngs[1][0], latlngs[1][1] - 2], latlngs[1],
-	'C', [latlngs[1][0], latlngs[1][1] + 2], [latlngs[2][0], latlngs[2][1] - 2], latlngs[2],
-	'C', [latlngs[2][0], latlngs[2][1] + 2], [latlngs[3][0], latlngs[3][1] - 2], latlngs[3],
-	'C', [latlngs[3][0], latlngs[3][1] + 2], [latlngs[4][0], latlngs[4][1] - 2], latlngs[4]
-	//'Q', latlngs[3], latlngs[4], latlngs[2]
-	//'T', latlngs[4]
-   ], {
-	   animate: 3000
-	   // ,
-	   // renderer: canvasRenderer
-   }).addTo(map);
-*/
 });
---
+++ src/utils.js
@@ -0,0 +1,119 @@
+import L from 'leaflet';
+
+export const CONTROL_POINTS = [[45.213004,-11.25],[40.178873,11.25],[47.517201,20.566406],[44.715514,27.949219],[47.398349,49.746094]];
+
+const types = ['Aircraft', 'Helicopter', 'Cruise missile', 'Drone'];
+
+export const prepareNodes = () => {
+		const cont = document.body;
+	const header = L.DomUtil.create('div', 'header', cont);
+	const leftMenu = L.DomUtil.create('div', 'leftMenu opened', cont);
+	const title = L.DomUtil.create('div', 'title', leftMenu);
+	title.innerHTML = 'Target types';
+	const lBody = L.DomUtil.create('div', 'body', leftMenu);
+	const targetsTitle = L.DomUtil.create('div', 'title', leftMenu);
+	targetsTitle.innerHTML = 'Targets';
+	const targetsNode = L.DomUtil.create('div', 'bodyTargets', leftMenu);
+	targetsNode._targets = [];
+	targetsNode._current;
+
+	let ship = L.DomUtil.create('div', 'icon ship hidden', cont);
+	var myIcon = L.icon({
+		iconSize: [24, 24],
+		iconUrl: './ship.svg'
+	});
+	const chkCurent = pNode => {
+		const _current = pNode._current;
+		[...pNode.childNodes].forEach((it, i) => {
+			it.classList[i === _current ? 'add' : 'remove']('current');
+		});
+	}
+
+	types.map(key => {
+		const bNode = L.DomUtil.create('div', '', lBody);
+		bNode.innerHTML = key;
+		L.DomEvent.on(bNode, 'mousedown', (ev) => {
+			ship.style.left = ev.clientX - 12 + 'px';
+			ship.style.top = ev.clientY - 12 + 'px';
+			L.DomUtil.setPosition(ship, L.point(0, 0));
+			ship.classList.remove('hidden');
+			// console.log('mousedown', ev);
+		});
+
+		const draggable = new L.Draggable(ship, bNode);
+		draggable
+			.on('dragend', ev => {
+				// console.log('dddd', ev);
+				const map = mapCont._map;
+				ship.classList.add('hidden');
+				const target = ev.target;
+				const p = target._newPos.add(target._startPoint).subtract(L.point(301, 42));
+				const latlng = map.containerPointToLatLng(p);
+				const node = L.DomUtil.create('div', '', targetsNode);
+				const dragStartTarget = ev.target._dragStartTarget;
+				let _cnt = dragStartTarget._cnt;
+				_cnt = !_cnt ? 1 : ++_cnt;
+				dragStartTarget._cnt = _cnt;
+				const title = dragStartTarget.innerHTML + '-' + _cnt;
+				node.innerHTML = title;
+				L.DomEvent.on(node, 'click', (ev) => {
+					targetsNode._current = undefined;
+					[...targetsNode.childNodes].forEach((it, i) => {
+						if (it === ev.target && !it.classList.contains('current')) {
+							it.classList.add('current');
+							targetsNode._current = i;
+							playButton.classList.remove('disabled');
+						} else {
+							it.classList.remove('current');
+						}
+					});
+					// if (targetsNode._current !== undefined) {
+						map._refreshCurves(targetsNode._current === undefined);
+					// }
+				});
+
+				targetsNode._current = targetsNode._targets.length;
+				targetsNode._targets.push(L.marker(latlng, {icon: myIcon, title: title}).addTo(map));
+				map._refreshCurves();
+				playButton.classList.remove('disabled');
+				chkCurent(targetsNode);
+			})
+			.enable();
+	});
+	const mapCont = L.DomUtil.create('div', 'map opened', cont);
+	const leftCont = L.DomUtil.create('span', 'left', header);
+	L.DomEvent.on(L.DomUtil.create('span', 'icon sidebar', leftCont), 'click', (ev) => {
+		const cList = leftMenu.classList;
+		if (cList.contains('opened')) {
+			cList.remove('opened');
+			mapCont.classList.remove('opened');
+		} else {
+			cList.add('opened');
+			mapCont.classList.add('opened');
+		}
+	});
+	const centerCont = L.DomUtil.create('span', 'center', header);
+	const playButton = L.DomUtil.create('span', 'icon play disabled', centerCont);
+	playButton.title = 'Просмотр';
+	const pauseButton = L.DomUtil.create('span', 'icon pause disabled', centerCont);
+	pauseButton.title = 'Пауза';
+	const rightCont = L.DomUtil.create('span', 'right', header);
+	// const playButton1 = L.DomUtil.create('span', 'icon play', rightCont);
+
+	L.DomEvent.on(pauseButton, 'click', (ev) => {
+		const cList = pauseButton.classList;
+		if (cList.contains('run')) {
+			cList.remove('run');
+		} else {
+			cList.add('run');
+		}
+	});
+
+	return {
+		mapCont: mapCont,
+		playButton: playButton,
+		pauseButton: pauseButton,
+		targets: targetsNode
+	};
+
+}
